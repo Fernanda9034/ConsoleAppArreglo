fe2cff4aa36cb3357962a2cae316d766c82bf4ed
Fernanda9034 <i21050345@monclova.tecnm.mx>
2021-10-19 11:25:28 -0500

    Add project files.

---
+++ ConsoleAppArreglo/Datos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArreglo
+{
+    class Datos
+    {
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        private string carrera;
+
+        public string Carrera
+        {
+            get { return carrera; }
+            set { carrera = value; }
+        }
+        public Datos()
+        {
+            nombre = "";
+            carrera = "";
+        }
+        public Datos(string nombre, string carrera)
+        {
+            this.nombre = nombre;
+            this.carrera = carrera;
+        }
+        public override string ToString()
+        {
+            return " Nombre:" + nombre + "Carrera:" + carrera;
+        }
+
+    }
+}
---
+++ ConsoleAppArreglo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArreglo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Datos[] a = new Datos[5];
+
+            //Console.WriteLine(" Ingresa el nombre: ");
+            //a.Nombre = Console.ReadLine();
+
+
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //  a[i] = "";
+            //}
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            // Console.WriteLine(a[i]);
+            //}
+            //Console.ReadKey()
+            string[] a = new string[5];
+            for (int i = 0; i < a.Length; i++)
+                {
+                    a[0] = ("Maria, ingenieria informatica");
+                    a[1] = ("Jose, ingenieria metalurgica");
+                    a[2] = ("Mario, licenciatura en educacion");
+                    a[3] = ("Carlos, licenciatura en enfermeria");
+                    a[4] = ("Azael, contaduria");
+                }
+                for (int i = 0; i < a.Length; i++)
+                {
+                    Console.WriteLine(a[i]);
+
+                }
+                Console.ReadKey();
+            
+
+  
+        }
+     
+    }
+}
